d9dd2f42a4cbe334378933e921c2b730e8233559
Aleydis <aleydis4@hotmail.com>
2025-02-01 18:31:29 -0600

    Add primary button component and update layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ButtonPrimary } from "@/components/buttonPrimary/page";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
+        <h1>No mames que estoy haciendo</h1>
+        <ButtonPrimary />
       </body>
     </html>
   );
---
+++ src/components/buttonPrimary/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { Button } from 'antd';
+
+export const ButtonPrimary = () => {
+  return (
+    <Button type="primary">Picale aquí</Button>
+  )
+
+}
+  
+   
+
